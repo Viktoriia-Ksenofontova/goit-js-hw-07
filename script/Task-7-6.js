b22181fdd0b7b718ce98998486b31ad23997f4d5
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2020-11-15 16:07:12 +0200

    Update Task-7-6.js

--- script/Task-7-6.js
+++ script/Task-7-6.js
@@ -4,8 +4,8 @@ validationInputRef.addEventListener('change', e =>{
   e.target.value.length === Number(e.target.dataset.length) ?
     e.target.classList.contains('invalid') ?
       e.target.classList.replace('invalid', 'valid') :
-    e.target.classList.add('valid') :
-    e.target.classList.add('invalid')
+      e.target.classList.add('valid') :
+    e.target.classList.add('invalid');
 
   
   //   if (e.target.value.length === Number(e.target.dataset.length)) { 
