c842c86362ce3b986cffafc6b52dc2f24d04826c
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2020-11-15 16:00:30 +0200

    Update Task-7-6.js

--- script/Task-7-6.js
+++ script/Task-7-6.js
@@ -1,12 +1,17 @@
 const validationInputRef = document.querySelector('#validation-input');
 
-validationInputRef.addEventListener('change', e => {
+validationInputRef.addEventListener('change', e =>{
+  e.target.value.length === Number(e.target.dataset.length) ?
+    e.target.classList.contains('invalid') ?
+      e.target.classList.replace('invalid', 'valid') :
+    e.target.classList.add('valid') :
+    e.target.classList.add('invalid')
+
   
-    if (e.target.value.length === Number(e.target.dataset.length)) { 
-      e.target.classList.add('valid');
-      e.target.classList.remove('invalid');
-  }
-    else {
-      e.target.classList.add('invalid');
-      e.target.classList.remove('valid');}
+  //   if (e.target.value.length === Number(e.target.dataset.length)) { 
+  //     e.target.classList.add('valid');
+  // }
+  //   else {
+  //     e.target.classList.add('invalid');
+  //     e.target.classList.remove('valid');}
 })
